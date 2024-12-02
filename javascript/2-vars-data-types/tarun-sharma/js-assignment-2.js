87af0f19cac8f4b2376209f8761fbcba7d2dba98
tarun-apty <tarun.sharma@apty.io>
2024-12-02 19:11:28 +0530

    completed js assignments 1 and 2

--- javascript/2-vars-data-types/tarun-sharma/js-assignment-2.js
+++
@@ -1,4 +0,0 @@
-var name = "John"; 
-let age = 30;    
-const country = "USA";
-
---
+++ javascript/2-vars-data-types/tarun-sharma/script/js-assignment-3.js
@@ -0,0 +1,8 @@
+function myConst(){
+    const name1 = "Smith"; 
+
+    const element = document.getElementById("string")
+    element.innerHTML = name1
+}
+
+myConst();
