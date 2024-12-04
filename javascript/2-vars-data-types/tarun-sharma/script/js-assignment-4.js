767c3cdb62a1ade8790940df24070ec0c160ee2b
tarun-apty <tarun.sharma@apty.io>
2024-12-04 19:01:25 +0530

    Added all combimations for type conversion and coercion

--- javascript/2-vars-data-types/tarun-sharma/script/js-assignment-4.js
+++ javascript/2-vars-data-types/tarun-sharma/script/js-assignment-4.js
@@ -1,45 +1,350 @@
-// string and number
-function myString(){
-
-    let num1 = 3;
-    let text1 = "767";
+//string start
+// conversion of string to number
+function myString1() {
+    let num1 = 3; 
+    let text1 = "hello"; 
 
     let result1 = Number(text1);
 
-    const element1 = document.getElementById("string")
+    const element1 = document.getElementById("string1");
     element1.innerHTML = result1;
 }
+myString1();
 
-myString();
+// conversion of string to boolean
+function myString2() {
+    let text2 = "hello"; 
 
-// boolean and undefined
-function myBoolean(){
+    let result2 = Boolean(text2); 
 
-    let num2 = true;
-    let text2
-
-    let result2 = Number(text2);
-
-    const element2 = document.getElementById("boolean")
+    const element2 = document.getElementById("string2");
     element2.innerHTML = result2;
 }
+myString2();
 
-myBoolean();
+// conversion of string to undefined
+function myString3() {
+    let text3 = "hello"; 
 
-// null and object
-function myObject(){
+    let result3;
 
-    let num3 = null;
-    const text3 = {
-        employee1: "tarun",
-        employee2: "sai",
-        employee3: "prem"
-    };
-
-    let result3 = Number(text3);
-
-    const element3 = document.getElementById("object")
+    const element3 = document.getElementById("string3");
     element3.innerHTML = result3;
 }
+myString3();
 
-myObject();
+// conversion of string to null
+function myString4() {
+    let text4 = "hello"; 
+
+    let result4 = null; 
+
+    const element4 = document.getElementById("string4");
+    element4.innerHTML = result4;
+}
+myString4();
+
+// conversion of string to object
+function myString5() {
+    let text5 = "hello"; 
+
+    let result5 = Object(text5); 
+    const element5 = document.getElementById("string5");
+    element5.innerHTML = result5;
+}
+myString5();
+
+//string end 
+//number start
+
+// conversion of number to string
+function myNumber1() {
+    let num6 = 3; 
+
+    let result6 = String(num6); 
+
+    const element6 = document.getElementById("number1");
+    element6.innerHTML = result6;
+}
+myNumber1();
+
+// conversion of number to boolean
+function myNumber2() {
+    let num7 = 3; 
+
+    let result7 = Boolean(num7); 
+
+    const element7 = document.getElementById("number2");
+    element7.innerHTML = result7;
+}
+myNumber2();
+
+// conversion of number to undefined
+function myNumber3() {
+    let num8 = 3; 
+
+    let result8; 
+
+    const element8 = document.getElementById("number3");
+    element8.innerHTML = result8;
+}
+myNumber3();
+
+// conversion of number to null
+function myNumber4() {
+    let num9 = 3; 
+
+    let result9 = null; 
+
+    const element9 = document.getElementById("number4");
+    element9.innerHTML = result9;
+}
+myNumber4();
+
+//conversion of number to object
+function myNumber5() {
+    let num10 = 3; 
+
+    let result10 = Object(num10); 
+
+    const element10 = document.getElementById("number5");
+    element10.innerHTML = result10;
+}
+myNumber5();
+
+//number end
+//boolean start
+
+// conversion of boolean to string
+function myBoolean1() {
+    let num11 = true; 
+
+    let result11 = String(num11); 
+
+    const element11 = document.getElementById("boolean1");
+    element11.innerHTML = result11;
+}
+myBoolean1();
+
+// conversion of boolean to number
+function myBoolean2() {
+    let num12 = true; 
+
+    let result12 = Number(num12); 
+
+    const element12 = document.getElementById("boolean2");
+    element12.innerHTML = result12;
+}
+myBoolean2();
+
+// conversion of boolean to undefined
+function myBoolean3() {
+    let num13 = true; 
+
+    let result13;
+
+    const element13 = document.getElementById("boolean3");
+    element13.innerHTML = result13;
+}
+myBoolean3();
+
+// conversion of boolean to null
+function myBoolean4() {
+    let num14 = true; 
+
+    let result14 = null; 
+
+    const element14 = document.getElementById("boolean4");
+    element14.innerHTML = result14;
+}
+myBoolean4();
+
+// conversion of boolean to object
+function myBoolean5() {
+    let num15 = true; 
+
+    let result15 = Object(num15); 
+
+    const element15 = document.getElementById("boolean5");
+    element15.innerHTML = result15;
+}
+myBoolean5();
+
+//boolean end
+//undefined start
+
+// conversion of undefined to string
+function myUndefined1() {
+    let num16 = undefined; 
+
+    const result16 = String(num16);
+
+    const element16 = document.getElementById("undefined1");
+    element16.innerHTML = result16;
+}
+myUndefined1();
+
+// conversion of undefined to number
+function myUndefined2() {
+    let num17 = undefined; 
+
+    const result17 = Number(num17); 
+
+    const element17 = document.getElementById("undefined2");
+    element17.innerHTML = result17;
+}
+myUndefined2();
+
+// conversion of undefined to boolean
+function myUndefined3() {
+    let num18 = undefined; 
+
+    const result18 = Boolean(num18);
+
+    const element18 = document.getElementById("undefined3");
+    element18.innerHTML = result18;
+}
+myUndefined3();
+
+// conversion of undefined to null
+function myUndefined4() {
+    let num20 = undefined; 
+
+    const result20 = null; 
+
+    const element20 = document.getElementById("undefined4");
+    element20.innerHTML = result20;
+}
+myUndefined4();
+
+// conversion of undefined to object
+function myUndefined5() {
+    let num21 = undefined; 
+
+    let result21 = Object(num21) ; 
+
+    const element21 = document.getElementById("undefined5");
+    element21.innerHTML = result21;
+}
+myUndefined5();
+
+//undefined end
+//null start
+
+// conversion of null to string
+function myNull1() {
+    let num22 = null; 
+
+    let result22 = String(num22); 
+
+    const element22 = document.getElementById("null1");
+    element22.innerHTML = result22;
+}
+myNull1();
+
+// conversion of null to number
+function myNull2() {
+    let num23 = null; 
+
+    let result23 = Number(num23);
+
+    const element23 = document.getElementById("null2");
+    element23.innerHTML = result23;
+}
+myNull2();
+
+// conversion of null to boolean
+function myNull3() {
+    let num24 = null; 
+
+    let result24 = Boolean(num24); 
+
+    const element24 = document.getElementById("null3");
+    element24.innerHTML = result24;
+}
+myNull3();
+
+// conversion of null to undefined
+function myNull4() {
+    let num25 = null; 
+
+    let result25; 
+
+    const element25 = document.getElementById("null4");
+    element25.innerHTML = result25;
+}
+myNull4();
+
+// conversion of null to object
+function myNull5() {
+    let num26 = null; 
+
+    let result26 = Object(num26); 
+
+    const element26 = document.getElementById("null5");
+    element26.innerHTML = result26;
+}
+myNull5();
+
+//null end
+//object start 
+
+// conversion of object to string
+function myObject1() {
+    const text27 = { name: "Alice", age: 30 }; 
+
+    let result27 = String(text27); 
+
+    const element27 = document.getElementById("object1");
+    element27.innerHTML = result27;
+}
+myObject1();
+
+// Conversion of object to number
+function myObject2() {
+    const text28 = { name: "Alice", age: 30 }; 
+
+    const result28 = Number(text28); 
+
+    const element28 = document.getElementById("object2");
+    element28.innerHTML = result28;
+}
+myObject2();
+
+// Conversion of object to boolean
+function myObject3() {
+    const text29 = { name: "Alice", age: 30 }; 
+
+    const result29 = Boolean(text29);
+
+    const element29 = document.getElementById("object3");
+    element29.innerHTML = result29;
+}
+myObject3();
+
+// Conversion of object to number
+function myObject4() {
+    const text30 = { name: "Alice", age: 30 }; 
+
+    const result30 = Number(text30);  
+
+    const element30 = document.getElementById("object4");
+    element30.innerHTML = result30;
+}
+myObject4();
+
+// Conversion of object to null
+function myObject5() {
+    const text31 = { name: "Alice", age: 30 }; 
+
+    const result31 = null;  
+
+    const element31 = document.getElementById("object5");
+    element31.innerHTML = result31;
+}
+myObject5();
+
+
+
+
+
--- javascript/2-vars-data-types/tarun-sharma/script/js-assignment-5.js
+++ javascript/2-vars-data-types/tarun-sharma/script/js-assignment-5.js
@@ -1,47 +1,406 @@
-// string & number
-function myString(){
+//string start
+// String and Number
+function myString1() {
+    let num1 = 5;
+    let text1 = "Hello";
 
-    let num1 = 2;    
-    let text1 = "How are you doing?";   
+    let result1 = num1 + text1;  
 
-    let result1 = num1 + text1;
-
-    const element1 = document.getElementById("string")
+    const element1 = document.getElementById("string1");
     element1.innerHTML = result1;
 }
 
-myString();
+myString1();
 
-// boolean and undefined
-function myBoolean(){
+// String and Boolean
+function myString2() {
+    let str = "Apty is a service based company ";
+    let boolVal = true;
 
-    let text2 = true;
-    let text3 
+    let result2 = str + boolVal;  
 
-    let result2 = text2 + text3;
-
-    const element2 = document.getElementById("boolean");
+    const element2 = document.getElementById("string2");
     element2.innerHTML = result2;
 }
 
-myBoolean();
+myString2();
 
-// null and object
-function myObject(){
+// String and Undefined
+function myString3() {
+    let str2 = "ajay";
+    let undefinedVal;
 
-    const name6 = {
-        employee1:"tarun", 
-        employee2:"sai", 
-        employee3:"Prem", 
-    };
-    
-    let text4 
+    let result3 = str2 + undefinedVal;  
 
-    let result3 = name6.employee1 + text4
-
-
-    const element3 = document.getElementById("object")
-    element3.innerHTML = result3
+    const element3 = document.getElementById("string3");
+    element3.innerHTML = result3;
 }
 
-myObject();
+myString3();
+
+// String and Null
+function myString4() {
+    let str3 = "ajay";
+    let nullVal = null;
+
+    let result4 = str3 + nullVal;  
+
+    const element4 = document.getElementById("string4");
+    element4.innerHTML = result4;
+}
+
+myString4();
+
+// String and Object
+function myString5() {
+    let str4 = "hello ";
+    let obj = { name: "laxman", age: 32 };
+
+    let result5 = str4 + obj;  
+
+    const element5 = document.getElementById("string5");
+    element5.innerHTML = result5;
+}
+
+myString5();
+
+//string end 
+//number start
+
+// Number and String
+function myNumber1() {
+    let num2 = 10;
+    let str5 = "The number is ";
+
+    let result6 = num2 + str5;  
+
+    const element6 = document.getElementById("number1");
+    element6.innerHTML = result6;
+}
+
+myNumber1();
+
+// Number and Boolean
+function myNumber2() {
+    let num3 = 8;
+    let boolVal2 = true;
+
+    let result7 = num3 + boolVal2;  
+    const element7 = document.getElementById("number2");
+    element7.innerHTML = result7;
+}
+
+myNumber2();
+
+// Number and Undefined
+function myNumber3() {
+    let num4 = 15;
+    let undefinedVal2;
+
+    let result8 = num4 + undefinedVal2;  
+
+    const element8 = document.getElementById("number3");
+    element8.innerHTML = result8;
+}
+
+myNumber3();
+
+// Number and Null
+function myNumber4() {
+    let num5 = 10;
+    let nullVal2 = null;
+
+    let result9 = num5 + nullVal2;  
+
+    const element9 = document.getElementById("number4");
+    element9.innerHTML = result9;
+}
+
+myNumber4();
+
+// Number and Object
+function myNumber5() {
+    let num6 = 20;
+    let obj2 = { key: "value" };
+
+    let result10 = num6 + obj2;  
+
+    const element10 = document.getElementById("number5");
+    element10.innerHTML = result10;
+}
+
+myNumber5();
+
+//number end 
+//boolean start
+
+// Boolean and String
+function myBoolean1() {
+    let boolVal3 = false;
+    let str6 = "Is it false? ";
+
+    let result11 = boolVal3 + str6;  
+
+    const element11 = document.getElementById("boolean1");
+    element11.innerHTML = result11;
+}
+
+myBoolean1();
+
+// Boolean and Number
+function myBoolean2() {
+    let boolVal4 = true;
+    let num7 = 12;
+
+    let result12 = boolVal4 + num7;  
+
+    const element12 = document.getElementById("boolean2");
+    element12.innerHTML = result12;
+}
+
+myBoolean2();
+
+// Boolean and Undefined
+function myBoolean3() {
+    let boolVal5 = false;
+    let undefinedVal3;
+
+    let result13 = boolVal5 + undefinedVal3;  
+
+    const element13 = document.getElementById("boolean3");
+    element13.innerHTML = result13;
+}
+
+myBoolean3();
+
+// Boolean and Null
+function myBoolean4() {
+    let boolVal6 = true;
+    let nullVal3 = null;
+
+    let result14 = boolVal6 + nullVal3;  
+
+    const element14 = document.getElementById("boolean4");
+    element14.innerHTML = result14;
+}
+
+myBoolean4();
+
+// Boolean and Object
+function myBoolean5() {
+    let boolVal7 = false;
+    let obj3 = { name: "Alice" };
+
+    let result15 = boolVal7 + obj3;  
+
+    const element15 = document.getElementById("boolean5");
+    element15.innerHTML = result15;
+}
+
+myBoolean5();
+
+//boolean end 
+//undefined start
+
+// Undefined and String
+function myUndefined1() {
+    let undefinedVal4;
+    let str7 = "Is this undefined? ";
+
+    let result16 = undefinedVal4 + str7;  
+
+    const element16 = document.getElementById("undefined1");
+    element16.innerHTML = result16;
+}
+
+myUndefined1();
+
+// Undefined and Number
+function myUndefined2() {
+    let undefinedVal5;
+    let num8 = 9;
+
+    let result17 = undefinedVal5 + num8;  
+
+    const element17 = document.getElementById("undefined2");
+    element17.innerHTML = result17;
+}
+
+myUndefined2();
+
+// Undefined and Boolean
+function myUndefined3() {
+    let undefinedVal6;
+    let boolVal8 = true;
+
+    let result18 = undefinedVal6 + boolVal8;  
+
+    const element18 = document.getElementById("undefined3");
+    element18.innerHTML = result18;
+}
+
+myUndefined3();
+
+// Undefined and Null
+function myUndefined4() {
+    let undefinedVal7;
+    let nullVal4 = null;
+
+    let result19 = undefinedVal7 + nullVal4;  
+
+    const element19 = document.getElementById("undefined4");
+    element19.innerHTML = result19;
+}
+
+myUndefined4();
+
+// Undefined and Object
+function myUndefined5() {
+    let undefinedVal8;
+    let obj4 = { name: "Bob" };
+
+    let result20 = undefinedVal8 + obj4; 
+
+    const element20 = document.getElementById("undefined5");
+    element20.innerHTML = result20;
+}
+
+myUndefined5();
+
+//undefined end
+//null start
+
+// Null and String
+function myNull1() {
+    let nullVal5 = null;
+    let str8 = "Null value: ";
+
+    let result21 = nullVal5 + str8;  
+
+    const element21 = document.getElementById("null1");
+    element21.innerHTML = result21;
+}
+
+myNull1();
+
+// Null and Number
+function myNull2() {
+    let nullVal6 = null;
+    let num9 = 5;
+
+    let result22 = nullVal6 + num9;  
+
+    const element22 = document.getElementById("null2");
+    element22.innerHTML = result22;
+}
+
+myNull2();
+
+// Null and Boolean
+function myNull3() {
+    let nullVal7 = null;
+    let boolVal9 = false;
+
+    let result23 = nullVal7 + boolVal9; 
+
+    const element23 = document.getElementById("null3");
+    element23.innerHTML = result23;
+}
+
+myNull3();
+
+// Null and Undefined
+function myNull4() {
+    let nullVal8 = null;
+    let undefinedVal9;
+
+    let result24 = nullVal8 + undefinedVal9;  
+
+    const element24 = document.getElementById("null4");
+    element24.innerHTML = result24;
+}
+
+myNull4();
+
+// Null and Object
+function myNull5() {
+    let nullVal9 = null;
+    let obj5 = { key: "value" };
+
+    let result25 = nullVal9 + obj5;  
+
+    const element25 = document.getElementById("null5");
+    element25.innerHTML = result25;
+}
+
+myNull5();
+
+//null end 
+//object start
+
+// Object and String
+function myObject1() {
+    let obj6 = { name: "Tom" };
+    let str9 = "Object is: ";
+
+    let result26 = obj6 + str9;  
+
+    const element26 = document.getElementById("object1");
+    element26.innerHTML = result26;
+}
+
+myObject1();
+
+// Object and Number
+function myObject2() {
+    let obj7 = { name: "ajay" };
+    let num10 = 5;
+
+    let result27 = obj7 + num10;  
+
+    const element27 = document.getElementById("object2");
+    element27.innerHTML = result27;
+}
+
+myObject2();
+
+// Object and Boolean
+function myObject3() {
+    let obj8 = { name: "laxman" };
+    let boolVal10 = true;
+
+    let result28 = obj8 + boolVal10; 
+
+    const element28 = document.getElementById("object3");
+    element28.innerHTML = result28;
+}
+
+myObject3();
+
+// Object and Undefined
+function myObject4() {
+    let obj9 = { name: "laxman" };
+    let undefinedVal10;
+
+    let result29 = obj9 + undefinedVal10; 
+
+    const element29 = document.getElementById("object4");
+    element29.innerHTML = result29;
+}
+
+myObject4();
+
+// Object and Null
+function myObject5() {
+    let obj10 = { name: "pravallika" };
+    let nullVal10 = null;
+
+    let result30 = obj10 + nullVal10; 
+
+    const element30 = document.getElementById("object5");
+    element30.innerHTML = result30;
+}
+
+myObject5();
+
+//object end
