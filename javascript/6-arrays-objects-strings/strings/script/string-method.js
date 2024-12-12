a3a02247efb9a2012d8d94eed0bde9a3e12e869d
Mudavathv123 <mudavathvinod49@gmail.com>
2024-12-12 14:19:59 +0530

    updated  in string method file

--- javascript/6-arrays-objects-strings/strings/script/string-method.js
+++ javascript/6-arrays-objects-strings/strings/script/string-method.js
@@ -4,8 +4,8 @@ document.getElementById('form').addEventListener('submit', evt => {
     evt.preventDefault();
 
     const text = document.getElementById('input').value.trim();
-    const result = text.split(' ').sort().join('');
+    const result = text.split('').sort().join('');
 
     document.getElementById('result').innerText = `After sort the string is : ${JSON.stringify(result)}`
-
+    document.getElementById('input').value = ""
 })
