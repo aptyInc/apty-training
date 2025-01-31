64e5854213d2e5e6150500496eedd2e1c599ae68
nagamanikantamanam <nagamanikanta.manam@apty.io>
2025-01-31 15:54:22 +0530

    maniknata add js-vras-data-types

---
+++ javascript/2-vars-data-types/manikanta/task3.js
@@ -0,0 +1,7 @@
+document.getElementById('str').innerHTML=typeof "hi";
+document.getElementById('num').innerHTML=typeof 9;
+document.getElementById('bol').innerHTML=typeof true;
+document.getElementById('un').innerHTML=typeof undefined;
+document.getElementById('nu').innerHTML=typeof null;
+document.getElementById('sy').innerHTML=typeof Symbol();
+document.getElementById('obj').innerHTML=typeof {'a':1};
